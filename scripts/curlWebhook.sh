a58fde3c1a7b5bbcea4a5ad5c78259cc8d4dec10
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-05-29 12:20:04 +0200

    Added github-webhook submodule

--- scripts/curlWebhook.sh
+++ scripts/curlWebhook.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -X POST http://github-webhook.local.pcfdev.io/ -d @/Users/marcingrzejszczak/repo/f2f/github-webhook/src/test/resources/github-webhook-input/issue-created.json \
+curl -X POST http://github-webhook.local.pcfdev.io/ -d @../github-webhook/src/test/resources/github-webhook-input/issue-created.json \
 --header "Content-Type: application/json"
